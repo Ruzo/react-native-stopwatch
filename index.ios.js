9d3ad79243873be713e48ef6aea5126bbf07dd82
Ruzo <richard@evigy.com>
2016-07-26 19:33:54 -0400

    Cleaned and fixed index files

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
   View
 } from 'react-native';
+import Timer from './components/timer';
+import LapList from './components/lap_list';
 
 class stopwatch extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      laps: []
+    }
+  }
+
+  updateLapList(lap){
+    this.setState({laps: [...this.state.laps, lap]});
+  }
+
+  resetLapList(){
+    this.setState({laps: []});
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Timer
+          updateLapList={this.updateLapList.bind(this)}
+          resetLapList={this.resetLapList.bind(this)}
+        />
+        <LapList
+          laps={this.state.laps}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('stopwatch', () => stopwatch);
